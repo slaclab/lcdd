0e36dfc4cd3cf010c0d8651b5f2468dcad36df76
Jeremy McCormick <jermccormick@gmail.com>
2014-05-08 23:24:03 +0000

    Change name of class to avoid naming clash with SLIC.

---
+++ include/lcdd/util/TimerUtil.hh
@@ -0,0 +1,58 @@
+#ifndef lcdd_util_TimerUtil_hh_
+#define lcdd_util_TimerUtil_hh_ 1
+
+#include <sys/time.h>
+
+#include <string>
+#include <map>
+#include <iostream>
+
+class TimerUtil {
+
+    static std::map<std::string, long> _timerMap;
+
+public:
+
+    static long getNanoTime() {
+        timespec ts;
+        clock_gettime(CLOCK_REALTIME, &ts);
+        return ts.tv_nsec;
+    }
+
+    static bool haveTimer(const std::string& name) {
+        return _timerMap.count(name) != 0;
+    }
+
+    static void startTimer(const std::string& name) {
+        _timerMap[name] = getNanoTime();
+    }
+
+    static long stopTimer(const std::string& name) {
+        _timerMap[name] = getNanoTime() - _timerMap[name];
+        return _timerMap[name];
+    }
+
+    static void addTime(const std::string& name, long nanos) {
+        if (!haveTimer(name))
+            createTimer(name);
+        _timerMap[name] += nanos;
+    }
+
+    static void createTimer(const std::string& name) {
+        _timerMap[name] = 0;
+    }
+
+    static long getTime(const std::string& name) {
+        return _timerMap[name];
+    }
+
+    static void printOut(const std::string& name, std::ostream& os) {
+        os << name << ": " << _timerMap[name] << std::endl;
+    }
+
+    static void clearTimers() {
+        _timerMap.clear();
+    }
+};
+
+#endif
---
+++ src/lcdd/util/TimerUtil.cc
@@ -0,0 +1,3 @@
+#include "lcdd/util/TimerUtil.hh"
+
+std::map<std::string, long> TimerUtil::_timerMap = std::map<std::string, long>();
